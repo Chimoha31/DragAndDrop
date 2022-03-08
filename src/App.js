dd42f3cc73a4ab3ffeb5790471540cb856935063
Chimoha31 <mokochii1108@gmail.com>
2022-03-08 15:23:44 -0800

    dnd api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
-import './App.css';
+import "./App.css";
+// drag and dropを実装するにはreact-beautiful-dndをインストールするする事！
+// ほぼ提携分なので、ドキュメントをみながらやるか暗記してしまった方が早い
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
+const items = [
+  {
+    id: "item-0",
+    content: "item 0",
+  },
+  {
+    id: "item-1",
+    content: "item 1",
+  },
+  {
+    id: "item-2",
+    content: "item 2",
+  },
+  {
+    id: "item-3",
+    content: "item 3",
+  },
+  {
+    id: "item-4",
+    content: "item 4",
+  },
+];
 
+// styling
+const grid = 8;
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  width: 250,
+  padding: grid,
+});
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid} 0`,
+  background: isDragging ? "lightgreen" : "grey", 
+  ...draggableStyle, 
+});
 
 function App() {
   return (
     <div>
+      <DragDropContext>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)} //dragしている間、cssのstyleを変える(背景色を変えたり)
+            >
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging, //dragしているのかを判断する。
+                        provided.draggableProps.style //滑らかにdragする機能。
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
